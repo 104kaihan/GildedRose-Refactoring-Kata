a2b4b37bd88cd2be388b6b2d2a8bd87107c761fa
kaihan <kaihan.chang@104.com.tw>
2022-01-18 15:39:56 +0800

    Refactor GildedRose use Item Conjured/Other

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -6,6 +6,8 @@ namespace GildedRose;
 
 use GildedRose\Item\Aged;
 use GildedRose\Item\Backstage;
+use GildedRose\Item\Conjured;
+use GildedRose\Item\Other;
 
 final class GildedRose
 {
@@ -40,74 +42,10 @@ final class GildedRose
             } elseif ($backstage) {
                 (new Backstage())->updateQuality($item);
             } elseif ($conjured) {
-                $this->conjuredUpdateQuality($item);
+                (new Conjured())->updateQuality($item);
             } else { // 其他 item
-                $this->otherUpdateQuality($item);
+                (new Other())->updateQuality($item);
             }
         }
-    }
-
-    /**
-     * Quality 会随着时间推移而提高 但`Quality`永远不会超过50
-     *
-     * @param $item
-     */
-    private function safeIncreaseQuality($item): void
-    {
-        if ($item->quality < 50) {
-            $item->quality += 1;
-        }
-    }
-
-    /**
-     * Quality 会随着时间推移而下降 但`Quality`永远不会为负值
-     *
-     * @param $item
-     */
-    private function safeDecreaseQuality($item): void
-    {
-        if ($item->quality > 0) {
-            $item->quality -= 1;
-        }
-    }
-
-    /**
-     * Conjured item : Quality 處理
-     *
-     * `Quality`下降速度比正常物品快一倍
-     *
-     * @param $item
-     */
-    private function conjuredUpdateQuality($item): void
-    {
-        $this->otherUpdateQuality($item);
-        $this->otherUpdateQuality($item);
-    }
-
-    /**
-     * 其他一般 item : Quality 處理
-     *
-     * @param $item
-     */
-    private function otherUpdateQuality($item): void
-    {
-        $this->safeDecreaseQuality($item);
-
-        // 品质`Quality`会以双倍速度加速下降
-        if ($this->isExpired($item)) {
-            $this->safeDecreaseQuality($item);
-        }
-    }
-
-    /**
-     * 销售期限过期
-     *
-     * @param $item
-     *
-     * @return bool true 過期
-     */
-    private function isExpired($item): bool
-    {
-        return $item->sell_in < 0;
     }
 }
