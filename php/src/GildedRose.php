cf97eba3032e513f2bb7f73cc846139d7b2e1dc9
kaihan <kaihan.chang@104.com.tw>
2022-01-18 12:04:02 +0800

    拔除 sulfuras 的判斷，因為他不用對 Quality 與 SellIn 做操作

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -23,6 +23,11 @@ final class GildedRose
             $backstage = $item->name === 'Backstage passes to a TAFKAL80ETC concert';
             $sulfuras = $item->name === 'Sulfuras, Hand of Ragnaros';
 
+            // "Sulfuras"（萨弗拉斯—炎魔拉格纳罗斯之手）永不过期，也不会降低品质`Quality`
+            if ($sulfuras) {
+                continue;
+            }
+
             // "Backstage passes"（后台通行证）与"Aged Brie"（陈年布利奶酪）类似，其品质`Quality`会随着时间推移而提高
             if ($aged) {
                 if ($item->quality < 50) {
@@ -31,11 +36,13 @@ final class GildedRose
             } elseif ($backstage) {
                 if ($item->quality < 50) {
                     $item->quality += 1;
+                    // 剩10天或更少的时候，品质`Quality`每天提高2
                     if ($item->sell_in < 11) {
                         if ($item->quality < 50) {
                             $item->quality += 1;
                         }
                     }
+                    // 5天或更少的时候，品质`Quality`每天提高3
                     if ($item->sell_in < 6) {
                         if ($item->quality < 50) {
                             $item->quality += 1;
@@ -44,23 +51,18 @@ final class GildedRose
                 }
             } else { // 其他 item
                 if ($item->quality > 0) {
-                    if (!$sulfuras) {
-                        $item->quality -= 1;
-                    }
+                    $item->quality -= 1;
                 }
             }
 
-            if (!$sulfuras) {
-                $item->sell_in -= 1;
-            }
+            $item->sell_in -= 1;
 
+            // 销售期限过期，品质`Quality`会以双倍速度加速下降
             if ($item->sell_in < 0) {
                 if (!$aged) {
                     if (!$backstage) {
                         if ($item->quality > 0) {
-                            if (!$sulfuras) {
-                                $item->quality -= 1;
-                            }
+                            $item->quality -= 1;
                         }
                     } else {
                         $item->quality -= $item->quality;
