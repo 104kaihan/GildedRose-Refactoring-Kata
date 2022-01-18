b610f5caa667cb8425ef54590775294c62983116
kaihan <kaihan.chang@104.com.tw>
2022-01-18 14:01:52 +0800

    把相同物件 Quality 處理整理在一起

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -30,9 +30,16 @@ final class GildedRose
 
             $item->sell_in -= 1;
 
+            // 销售期限过期
+            $expired = $item->sell_in < 0;
+
             // "Backstage passes"（后台通行证）与"Aged Brie"（陈年布利奶酪）类似，其品质`Quality`会随着时间推移而提高
             if ($aged) {
                 $this->safeIncreaseQuality($item);
+
+                if ($expired) {
+                    $this->safeIncreaseQuality($item);
+                }
             } elseif ($backstage) {
                 $this->safeIncreaseQuality($item);
                 // 剩10天或更少的时候，品质`Quality`每天提高2
@@ -43,22 +50,15 @@ final class GildedRose
                 if ($item->sell_in < 5) {
                     $this->safeIncreaseQuality($item);
                 }
-            } else { // 其他 item
-                $this->safeDecreaseQuality($item);
-            }
 
-            // 销售期限过期
-            $expired = $item->sell_in < 0;
-            if ($aged) {
-                if ($expired) {
-                    $this->safeIncreaseQuality($item);
-                }
-            } elseif ($backstage) {
                 if ($expired) {
                     // 一旦过期，品质就会降为0
                     $item->quality -= $item->quality;
                 }
-            } else { // 其他 item，品质`Quality`会以双倍速度加速下降
+            } else { // 其他 item
+                $this->safeDecreaseQuality($item);
+
+                // 品质`Quality`会以双倍速度加速下降
                 if ($expired) {
                     $this->safeDecreaseQuality($item);
                 }
