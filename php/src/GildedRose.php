1fb916b7ce6459d442dfcc9221d7c1863237709c
kaihan <kaihan.chang@104.com.tw>
2022-01-18 12:34:32 +0800

    Merge else if

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -49,10 +49,8 @@ final class GildedRose
                         }
                     }
                 }
-            } else { // 其他 item
-                if ($item->quality > 0) {
-                    $item->quality -= 1;
-                }
+            } elseif ($item->quality > 0) { // 其他 item
+                $item->quality -= 1;
             }
 
             $item->sell_in -= 1;
@@ -63,14 +61,11 @@ final class GildedRose
                     if ($item->quality < 50) {
                         $item->quality += 1;
                     }
-                } else {
-                    if ($backstage) {
-                        $item->quality -= $item->quality;
-                    } else {
-                        if ($item->quality > 0) {
-                            $item->quality -= 1;
-                        }
-                    }
+                } elseif ($backstage) {
+                    // 旦过期，品质就会降为0
+                    $item->quality -= $item->quality;
+                } elseif ($item->quality > 0) { // 其他 item
+                    $item->quality -= 1;
                 }
             }
         }
